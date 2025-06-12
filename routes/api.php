50a819a5b6bbdd0d9edd745fe5778a26c4cf3f90
Swornima Kc <swornimakc08@gmail.com>
2025-06-12 07:45:38 +0545

    feat: add logn module ; add register module

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,25 @@
 <?php
 
 use Illuminate\Http\Request;
-
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::get('hello',function(Request $request){
-    return 'test hello';
-});
+Route::get('hello' ,function (Request $request){
+    return'test hello';
 
-Route::post('/register',[AuthController::class,'register']);
+Route::post('/register', [AuthController::class, 'register']);
+
+}
+);
+use App\Http\Controllers\Api\Auth\RegisterController;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/profile',[Authcontroller::class,'getProfile'])->middleware('auth:sanctum');
